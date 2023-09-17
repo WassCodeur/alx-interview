c427ca6d46d7be8297da173894d5ddbcc825c301
WassCodeur <wachioubouraima56@gmail.com>
2023-09-17 20:33:59 +0000

    update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+
+
+def pascal_triangle(n):
+    father = [[]]
+    child = []
+    if n <= 0:
+        return []
+    else:
+        for i in range(n):
+            father[i] = child
+            
