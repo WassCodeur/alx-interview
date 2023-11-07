2bb43f5bb70c847f1761bc2f9b2251aef2ad91be
WassCodeur <wachioubouraima56@gmail.com>
2023-11-07 20:26:25 +0000

    setup

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""UTF-8 Validation"""
+
+from typing import List
+
+
+def validUTF8(data: List[int]) -> bool:
+    """check valid utf8
+
+    Args
+    data: List of int
+
+    Return
+    bool
+    """
+
