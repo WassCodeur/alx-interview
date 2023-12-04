4555576ab3654068120fd9079d6d7d0ae3140d01
WassCodeur <wachioubouraima56@gmail.com>
2023-12-04 01:38:40 +0000

    island perimeter V1

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,18 +13,15 @@ def island_perimeter(grid):
     ------
     None
     """
-    heigh = 1
-    width = 1
+    per = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if (i + 1) < (len(grid) - 1):
-                    if grid[i + 1][j]:
-                        heigh += 1
+                per += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    per -= 2
 
-                    if (j + 1) < (len(grid[i])):
-                        width += 1
-
-    per = (heigh + width) * 2
+                if j > 0 and grid[i][j - 1]:
+                    per -= 2
 
     return (per)
